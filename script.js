05990c83a6983479258332bc6ee30efa665ae3ea
ReyChancla <61477102+ReyChancla@users.noreply.github.com>
2022-01-27 19:01:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,15 @@
-var ancho = document.getElementById("ancho").value,
-    largo = document.getElementById("largo").value,
-    alto = document.getElementById("alto").value;
+'use strict'
 
-function calcular() {
-    let ancho = parseInt(document.getElementById("ancho").value),
-        largo = parseInt(document.getElementById("largo").value),
-        alto = parseInt(document.getElementById("alto").value),
-        x = (ancho *2) +(largo *2),
-        y = (alto) +(largo *2);
-        
-    var CanalE = document.getElementById("E"),
-        CanalB = document.getElementById("B"),
-        CanalC = document.getElementById("C"),
-        CanalBC = document.getElementById("BC"),
-        CanalEB = document.getElementById("EB"),
-        Canales = document.getElementById("Canales").value;
-    /*
-    E = 2;
-    B = 3;
-    C = 4;
-    BC = 6;
-    EB = 5,
-    */
-    
-    CanalesDos =parseInt(Canales);
-       
-        
-    console.log(ancho);
-    console.log(largo);
-    console.log(alto);
+const bloque = document.querySelectorAll('.Indice');
+const Indice = document.querySelectorAll('.bloque');
 
-    console.log(ancho +largo +alto);
-    
-    console.log(x);
-    console.log(y);
-
-    document.getElementById("Tamaño_de_plancha").innerHTML = x+("x")+y;
-
-    console.log(CanalesDos);
-
-    if (CanalesDos == 6) {
-        alert("Cuidado la pestaña debe de ser de 40 cm");
-    } else if (CanalesDos == 5) {
-        alert("Cuidado la pestaña debe de ser de 40 cm");
-    } else {
-        console.log("Pestaña normal de 30 cm");
-    }{
-        
-    }
-  
-
-}
+bloque.forEach( (cadaBloque , i) => {
+    bloque[i].addEventListener('click', () =>{
+        bloque.forEach( ( cadaBloque , i)=>{
+            bloque[i].classList.remove('activo')
+            Indice[i].classList.remove('activo')
+        })
+        bloque[i].classList.add('activo')
+        Indice[i].classList.add('activo')
+    })
+});
